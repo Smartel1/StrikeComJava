5c8f86652a47e67b53abc1113125c4edf86f200a
drews <as@bstd.ru>
2019-10-17 10:18:44 +0500

    Minor/rename conflict reasons

---
+++ src/main/resources/db/migration/V1_2__rename_conflict_reasons.sql
@@ -0,0 +1,5 @@
+UPDATE public.conflict_reasons SET name_en = 'Paycheck delay' WHERE name_en = 'Delay salary';
+UPDATE public.conflict_reasons SET name_en = 'Cuts' WHERE name_en = 'Contraction';
+UPDATE public.conflict_reasons SET name_en = 'Termination' WHERE name_en = 'Dismissal';
+UPDATE public.conflict_reasons SET name_en = 'Management policy' WHERE name_en = 'Management course';
+UPDATE public.conflict_reasons SET name_en = 'Working hours' WHERE name_en = 'Working time';
