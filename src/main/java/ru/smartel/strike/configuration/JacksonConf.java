dff8cf867ac660038e2b47649745899940b92e8d
drews <as@bstd.ru>
2019-11-06 12:28:52 +0500

    camelCase requests

--- src/main/java/ru/smartel/strike/configuration/JacksonConf.java
+++ src/main/java/ru/smartel/strike/configuration/JacksonConf.java
@@ -1,8 +1,6 @@
 package ru.smartel.strike.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +10,6 @@ public class JacksonConf {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
         mapper.findAndRegisterModules();
         return mapper;
     }
