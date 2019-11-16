309217ce8183034caf6dd0fc72f58c098dfe2966
Andrew <40439395+Smartel1@users.noreply.github.com>
2019-11-16 23:40:10 +0500

    bugfix/fix view endpoints (#47)

--- src/main/java/ru/smartel/strike/controller/EventController.java
+++ src/main/java/ru/smartel/strike/controller/EventController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.smartel.strike.dto.request.event.EventCreateRequestDTO;
 import ru.smartel.strike.dto.request.event.EventListRequestDTO;
+import ru.smartel.strike.dto.request.event.EventShowDetailRequestDTO;
 import ru.smartel.strike.dto.request.event.EventUpdateRequestDTO;
 import ru.smartel.strike.dto.response.ListWrapperDTO;
 import ru.smartel.strike.dto.response.event.EventDetailDTO;
@@ -43,12 +44,8 @@ public class EventController {
     }
 
     @GetMapping("{id}")
-    public EventDetailDTO show(
-            @PathVariable("locale") Locale locale,
-            @PathVariable("id") long eventId,
-            @RequestParam(value = "withRelatives", required = false) boolean withRelatives
-    ) {
-        return eventService.incrementViewsAndGet(eventId, locale, withRelatives);
+    public EventDetailDTO show(EventShowDetailRequestDTO dto) {
+        return eventService.incrementViewsAndGet(dto);
     }
 
     @PostMapping("{id}/favourites")
