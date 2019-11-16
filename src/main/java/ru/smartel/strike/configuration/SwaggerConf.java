bb1b28b51d948d09304aed3e785e8e793d3a0130
drews <as@bstd.ru>
2019-11-16 20:02:25 +0500

    issue_40: ignore auth principal

--- src/main/java/ru/smartel/strike/configuration/SwaggerConf.java
+++ src/main/java/ru/smartel/strike/configuration/SwaggerConf.java
@@ -2,6 +2,7 @@ package ru.smartel.strike.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -17,6 +18,7 @@ public class SwaggerConf {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .ignoredParameterTypes(AuthenticationPrincipal.class);
     }
 }
