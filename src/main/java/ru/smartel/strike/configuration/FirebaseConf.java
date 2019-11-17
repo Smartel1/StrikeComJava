f453baa4236122f61c171d24785bb83450b072dc
drews <as@bstd.ru>
2019-11-17 19:19:49 +0500

    issue_29: push service

--- src/main/java/ru/smartel/strike/configuration/FirebaseConf.java
+++ src/main/java/ru/smartel/strike/configuration/FirebaseConf.java
@@ -3,7 +3,9 @@ package ru.smartel.strike.configuration;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -30,5 +32,10 @@ public class FirebaseConf implements WebMvcConfigurer {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Bean
+    public FirebaseMessaging messaging() {
+        return FirebaseMessaging.getInstance();
     }
 }
