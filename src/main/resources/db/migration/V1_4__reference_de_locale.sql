64982bc27382f1b3831f8e85aaf7282cccf5f51e
drews <as@bstd.ru>
2019-11-24 18:06:14 +0500

    issue_35: some references translation

---
+++ src/main/resources/db/migration/V1_4__reference_de_locale.sql
@@ -0,0 +1,42 @@
+UPDATE public.event_statuses SET name_de = 'Entwicklung' WHERE name_en = 'Development';
+UPDATE public.event_statuses SET name_de = 'Fertigstellung' WHERE name_en = 'Completion';
+UPDATE public.event_statuses SET name_de = 'Neu' WHERE name_en = 'New';
+
+UPDATE public.event_types SET name_de = 'Beschwerde' WHERE name_en = 'Appeal';
+UPDATE public.event_types SET name_de = 'Demonstration' WHERE name_en = 'Demonstration';
+UPDATE public.event_types SET name_de = 'Hungerstreik' WHERE name_en = 'Hunger strike';
+UPDATE public.event_types SET name_de = 'Streik' WHERE name_en = 'Strike';
+UPDATE public.event_types SET name_de = 'Konflikt' WHERE name_en = 'Clash';
+UPDATE public.event_types SET name_de = 'Drohung' WHERE name_en = 'Threat';
+UPDATE public.event_types SET name_de = 'Artkl. 142' WHERE name_en = 'st. 142';
+UPDATE public.event_types SET name_de = 'Verhandlungen' WHERE name_en = 'Conversation';
+UPDATE public.event_types SET name_de = 'Teilstreik' WHERE name_en = 'Partial strike';
+UPDATE public.event_types SET name_de = 'Stressensperrung' WHERE name_en = 'Highway overlap';
+UPDATE public.event_types SET name_de = 'Gericht' WHERE name_en = 'Court';
+UPDATE public.event_types SET name_de = 'Ermittlung' WHERE name_en = 'Investigation';
+UPDATE public.event_types SET name_de = 'Ablehnung von Ueberstunden' WHERE name_en = 'Refusal of processing';
+UPDATE public.event_types SET name_de = 'Massenentlassung' WHERE name_en = 'Mass layoff';
+
+UPDATE public.conflict_results SET name_de = 'Zufriedenstellend' WHERE name_en = 'Completely satisfied';
+UPDATE public.conflict_results SET name_de = 'Teilweise zufriedenstellend' WHERE name_en = 'Partially satisfied';
+UPDATE public.conflict_results SET name_de = 'Nicht zufriedenstellend' WHERE name_en = 'Not satisfied';
+
+UPDATE public.countries SET name_de = 'Russland' WHERE name_en = 'Russia';
+UPDATE public.countries SET name_de = 'Aserbaidschan' WHERE name_en = 'Azerbaijan';
+UPDATE public.countries SET name_de = 'Armenien' WHERE name_en = 'Armenia';
+UPDATE public.countries SET name_de = 'Weißrussland' WHERE name_en = 'Belarus';
+UPDATE public.countries SET name_de = 'Kasachstan' WHERE name_en = 'Kazakhstan';
+UPDATE public.countries SET name_de = 'Kirgisistan' WHERE name_en = 'Kyrgyzstan';
+UPDATE public.countries SET name_de = 'Moldawien' WHERE name_en = 'Moldova';
+UPDATE public.countries SET name_de = 'Tadschikistan' WHERE name_en = 'Tajikistan';
+UPDATE public.countries SET name_de = 'Usbekistan' WHERE name_en = 'Uzbekistan';
+UPDATE public.countries SET name_de = 'Georgia' WHERE name_en = 'Georgia';
+UPDATE public.countries SET name_de = 'Ukraine' WHERE name_en = 'Ukraine';
+UPDATE public.countries SET name_de = 'Turkmenistan' WHERE name_en = 'Turkmenistan';
+UPDATE public.countries SET name_de = 'Litauen' WHERE name_en = 'Lithuania';
+UPDATE public.countries SET name_de = 'Lettland' WHERE name_en = 'Latvia';
+UPDATE public.countries SET name_de = 'Estland' WHERE name_en = 'Estonia';
+UPDATE public.countries SET name_de = 'Die Republik Korea' WHERE name_en = 'The Republic of Corea';
+UPDATE public.countries SET name_de = 'Kirgisistan' WHERE name_en = 'Kyrgyzstan';
+UPDATE public.countries SET name_de = 'Mongolei' WHERE name_en = 'Mongolia';
+UPDATE public.countries SET name_de = 'Truthahn' WHERE name_en = 'Turkey';
