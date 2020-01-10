e961817c9f97d75b9b42b7b8f6a5356b31fce545
Andrew <smartel93@mail.ru>
2020-01-10 22:04:11 +0500

    upd bundle

--- src/main/java/ru/smartel/strike/controller/PageController.java
+++ src/main/java/ru/smartel/strike/controller/PageController.java
@@ -1,12 +1,18 @@
 package ru.smartel.strike.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+    @Value("${apiUrl}")
+    private String apiUrl;
+
     @GetMapping("moderation")
-    public String moderation() {
+    public String moderation(Model model) {
+        model.addAttribute("apiUrl", apiUrl);
         return "moderation";
     }
 }
