d46d73cf7de6bbd8c14c224a5b0abf4ba4edf286
Andrew <smartel93@mail.ru>
2020-01-19 19:52:59 +0500

    fix german translations

---
+++ src/main/resources/db/migration/V1_11__fix_german_translations.sql
@@ -0,0 +1,8 @@
+UPDATE event_types SET name_de = 'Ablehnung von Überstunden' WHERE name_en = 'Refusal of processing';
+UPDATE countries SET name_de = 'Türkei' WHERE name_en = 'Turkey';
+UPDATE industries SET name_de = 'Baugewerbe' WHERE name_en = 'Building';
+UPDATE industries SET name_de = 'Verkehrswesen' WHERE name_en = 'Transport';
+UPDATE industries SET name_de = 'Handel' WHERE name_en = 'Trading';
+UPDATE industries SET name_de = 'Fertigungsindustrie' WHERE name_en = 'Manufacturing industries';
+UPDATE industries SET name_de = 'Wohn- und kommunale Dienstleistungen' WHERE name_en = 'Housing and communal services';
+UPDATE industries SET name_de = 'Bildung und Wissenschaft' WHERE name_en = 'Education and science';
