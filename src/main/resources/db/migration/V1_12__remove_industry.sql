e814d2acf27b58d671f172b915b2b6e7d72a24b3
Andrew <smartel93@mail.ru>
2020-01-29 15:36:42 +0500

    fix migration

--- src/main/resources/db/migration/V1_12__remove_industry.sql
+++ src/main/resources/db/migration/V1_12__remove_industry.sql
@@ -1,3 +1,2 @@
-update conflicts set industry_id = (select id from industries where name_ru='Обрабатывающие производства')
-where industry_id = (select id from industries where name_ru='Электроэнергетика')
-delete from industries where name_ru='Электроэнергетика'
+update conflicts set industry_id = (select id from industries where name_ru='Обрабатывающие производства') where industry_id = (select id from industries where name_ru='Электроэнергетика');
+delete from industries where name_ru='Электроэнергетика';
