cf531ae4948f1abae29eac61e7bbdc0dede3bf32
Siliutin Andrei <siliutin_a@utkonos.ru>
2020-04-22 10:09:46 +0300

    remove hibernate conf

--- src/main/java/ru/smartel/strike/StrikeApplication.java
+++ src/main/java/ru/smartel/strike/StrikeApplication.java
@@ -2,15 +2,12 @@ package ru.smartel.strike;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
-@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class) //exclude позволяет избавиться от исключений (хз как работает)
+@SpringBootApplication
 public class StrikeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StrikeApplication.class, args);
 	}
-
 }
