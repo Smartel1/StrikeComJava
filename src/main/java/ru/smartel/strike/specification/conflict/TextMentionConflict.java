ee3140ddc19a053afe05d112df0cc91333362bfc
Siliutin Andrey <siliutin_a@utkonos.ru>
2020-06-24 21:32:13 +0500

    change filter

--- src/main/java/ru/smartel/strike/specification/conflict/TextMentionConflict.java
+++ src/main/java/ru/smartel/strike/specification/conflict/TextMentionConflict.java
@@ -4,40 +4,40 @@ import org.springframework.data.jpa.domain.Specification;
 import ru.smartel.strike.entity.Conflict;
 import ru.smartel.strike.entity.Event;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.JoinType;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
+
+import static java.util.Objects.isNull;
 
 /**
  * Conflicts which have specified text in title or which have events containing this text in contents/titles
  */
 public class TextMentionConflict implements Specification<Conflict> {
-    private String desiredContent;
+    private final String desiredContent;
 
     public TextMentionConflict(String desiredContent) {
-        this.desiredContent = "%" + desiredContent.toLowerCase() + "%";
+        this.desiredContent = desiredContent;
     }
 
     @Override
     public Predicate toPredicate(Root<Conflict> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-        if (desiredContent.equals("%%")) return null;
+        if (isNull(desiredContent) || desiredContent.isBlank()) return null;
+
+        var desiredContentQuery = "%" + desiredContent.toLowerCase() + "%";
         Join<Conflict, Event> join = root.join("events", JoinType.LEFT);
         return cb.or(
-                cb.like(cb.lower(root.get("titleRu")), desiredContent),
-                cb.like(cb.lower(root.get("titleEn")), desiredContent),
-                cb.like(cb.lower(root.get("titleEs")), desiredContent),
-                cb.like(cb.lower(root.get("titleDe")), desiredContent),
-                cb.like(cb.lower(join.get("post").get("titleRu")), desiredContent),
-                cb.like(cb.lower(join.get("post").get("titleEn")), desiredContent),
-                cb.like(cb.lower(join.get("post").get("titleEs")), desiredContent),
-                cb.like(cb.lower(join.get("post").get("titleDe")), desiredContent),
-                cb.like(cb.lower(join.get("post").get("contentRu")), desiredContent),
-                cb.like(cb.lower(join.get("post").get("contentEn")), desiredContent),
-                cb.like(cb.lower(join.get("post").get("contentEs")), desiredContent),
-                cb.like(cb.lower(join.get("post").get("contentDe")), desiredContent)
+                cb.like(cb.lower(root.get("titleRu")), desiredContentQuery),
+                cb.like(cb.lower(root.get("titleEn")), desiredContentQuery),
+                cb.like(cb.lower(root.get("titleEs")), desiredContentQuery),
+                cb.like(cb.lower(root.get("titleDe")), desiredContentQuery),
+                cb.like(cb.lower(root.get("companyName")), desiredContentQuery),
+                cb.like(cb.lower(join.get("post").get("titleRu")), desiredContentQuery),
+                cb.like(cb.lower(join.get("post").get("titleEn")), desiredContentQuery),
+                cb.like(cb.lower(join.get("post").get("titleEs")), desiredContentQuery),
+                cb.like(cb.lower(join.get("post").get("titleDe")), desiredContentQuery),
+                cb.like(cb.lower(join.get("post").get("contentRu")), desiredContentQuery),
+                cb.like(cb.lower(join.get("post").get("contentEn")), desiredContentQuery),
+                cb.like(cb.lower(join.get("post").get("contentEs")), desiredContentQuery),
+                cb.like(cb.lower(join.get("post").get("contentDe")), desiredContentQuery)
         );
     }
 }
